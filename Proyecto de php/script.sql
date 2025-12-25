b1109760c397e1a689324752f75a43d7ee752d54
Carlos Páez <carlospaezguerra@gmail.com>
2025-12-25 14:17:20 -0400

    correciones de db

--- Proyecto de php/script.sql
+++ Proyecto de php/script.sql
@@ -1,22 +1,13 @@
-create table admins
-(
-    id       int auto_increment
-        primary key,
-    nombre   varchar(30) null,
-    apellido varchar(30) null,
-    alias    varchar(30) null,
-    pass     varchar(5)  null
-)
-    engine = InnoDB;
-
 create table usuarios
 (
-    id       int auto_increment
+    id               int auto_increment
         primary key,
-    nombre   varchar(30) null,
-    apellido varchar(30) null,
-    pass     varchar(5)  null
-)
-    engine = InnoDB;
+    nombre           varchar(30)  null,
+    apellido         varchar(30)  null,
+    email            varchar(50)  null,
+    fecha_nacimiento datetime     null,
+    user_level       varchar(10)  null,
+    pass             varchar(255) null
+);
 
 
