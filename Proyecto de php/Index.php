a856d7dbaf3a8465284f5ea285db3e6205b8c1fe
Carlos Páez <carlospaezguerra@gmail.com>
2025-12-29 15:48:55 -0400

    correccion de nombre

--- Proyecto de php/Index.php
+++ Proyecto de php/Index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Practicas de PHP</title>
+    <title>Optimized time</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/read.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
